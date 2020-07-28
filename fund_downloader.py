3ed2b101f50d6a193a948b1b8aacaf2d04fa7539
风之凌殇 <fzls.zju@gmail.com>
2020-07-28 10:33:39 +0800

    1. 更新cookie 2. 增加下载重试

--- fund_downloader.py
+++ fund_downloader.py
@@ -17,11 +17,11 @@ headers = {
     "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7,en-GB;q=0.6,ja;q=0.5",
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
-    "Cookie": "qgqp_b_id=bb4ded2cbf6298d0f64987e0dbe18f38; EMFUND1=null; EMFUND2=null; EMFUND3=null; EMFUND4=null; EMFUND5=null; EMFUND6=null; EMFUND7=null; EMFUND8=null; EMFUND0=null; st_si=00114758560421; st_asi=delete; EMFUND9=08-30 19:17:51@#$%u6613%u65B9%u8FBE%u6D88%u8D39%u884C%u4E1A%u80A1%u7968@%23%24110022; st_pvi=55245435666572; st_sp=2019-04-01%2011%3A39%3A25; st_inirUrl=https%3A%2F%2Fwww.baidu.com%2Flink; st_sn=6; st_psi=20190830191751422-0-5230010871",
+    "Cookie": "ASP.NET_SessionId=ifqhxwf1tuwcui1rh4o2evdm; st_si=68354466271833; st_asi=delete; _adsame_fullscreen_16928=1; FundWebTradeUserInfo=JTdCJTIyQ3VzdG9tZXJObyUyMjolMjIlMjIsJTIyQ3VzdG9tZXJOYW1lJTIyOiUyMiUyMiwlMjJWaXBMZXZlbCUyMjolMjIlMjIsJTIyTFRva2VuJTIyOiUyMiUyMiwlMjJJc1Zpc2l0b3IlMjI6JTIyJTIyLCUyMlJpc2slMjI6JTIyJTIyJTdE; qgqp_b_id=30d909200d3019b496a281498c587c9a; _adsame_fullscreen_18503=1; EMFUND1=null; EMFUND2=null; EMFUND3=null; EMFUND4=null; EMFUND5=null; EMFUND6=null; EMFUND7=null; EMFUND0=null; EMFUND8=07-27%2016%3A36%3A00@%23%24%u4FE1%u8FBE%u6FB3%u94F6%u65B0%u80FD%u6E90%u4EA7%u4E1A%u80A1%u7968@%23%24001410; EMFUND9=07-27 16:36:11@#$%u56FD%u6CF0%u56FD%u8BC1%u6709%u8272%u91D1%u5C5E%u884C%u4E1A%u5206%u7EA7B@%23%24150197; st_pvi=55245435666572; st_sp=2019-04-01%2011%3A39%3A25; st_inirUrl=https%3A%2F%2Fwww.baidu.com%2Flink; st_sn=20; st_psi=20200727163640224-0-2607014817",
     "DNT": "1",
-    "Host": "api.fund.eastmoney.com",
+    "Host": "fund.eastmoney.com",
     "If-None-Match": "b56a685e-f638-4ccc-820d-c0495e577277",
-    "Referer": "http://fundf10.eastmoney.com/jjjz_110022.html",
+    "Referer": "http://fund.eastmoney.com/data/fundranking.html",
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
 }
@@ -51,7 +51,15 @@ class FundDownloader:
         self._download_data()
 
     def _download_data(self):
-        res = requests.get(self.url, headers=headers)
+        res = None
+        for attempt in range(20):
+            try:
+                res = requests.get(self.url, headers=headers)
+            except Exception as e:
+                print("error during requests.get({}), attempt={}, e={}".format(self.url, attempt, e))
+            else:
+                break
+
         left_index = res.text.find("(")
         right_index = res.text.rfind(")")
         data = json.loads(res.text[left_index + 1:right_index])
@@ -88,7 +96,7 @@ class AllFundDownloader:
     funds = []
 
     def __init__(self):
-        res = requests.get(self.get_all_funds_api)
+        res = requests.get(self.get_all_funds_api, headers=headers)
         left = "datas:"
         right = ",allRecords"
         left_index = res.text.find(left)
