399c9a2d1cd4f69f89115ba0807baadb8f5bd8d6
风之凌殇 <fzls.zju@gmail.com>
2025-04-21 23:01:15 +0800

    feat: 数据不再支持一次性全部拉取，改成分页拉取

--- fund_downloader.py
+++ fund_downloader.py
@@ -16,18 +16,18 @@ import requests
 
 # 若过期，则参考 https://fundf10.eastmoney.com/jjjz_000216.html 页面的请求，更新cookie
 headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "Accept-Encoding": "gzip, deflate",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
     "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7,en-GB;q=0.6,ja;q=0.5",
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
-    "Cookie": "qgqp_b_id=2678f31d9266f2a387bfd72a2eb6ae0d; AUTH_FUND.EASTMONEY.COM_GSJZ=AUTH*TTJJ*TOKEN; HAList=ty-100-N225-%u65E5%u7ECF225; EMFUND1=null; EMFUND2=null; EMFUND3=null; EMFUND4=null; EMFUND5=null; EMFUND6=null; EMFUND0=null; EMFUND8=02-05%2019%3A36%3A48@%23%24%u534E%u5B89%u65E5%u7ECF225ETF@%23%24513880; EMFUND9=02-05%2019%3A37%3A07@%23%24%u6613%u65B9%u8FBE%u65E5%u7ECF225ETF@%23%24513000; st_si=92844999740212; st_asi=delete; EMFUND7=02-11 16:11:28@#$%u534E%u5B89%u9EC4%u91D1%u6613ETF%u8054%u63A5A@%23%24000216; st_pvi=34726450000364; st_sp=2023-04-16%2020%3A01%3A45; st_inirUrl=http%3A%2F%2Ffund.eastmoney.com%2Fdata%2Ffundranking.html; st_sn=7; st_psi=20240211161221905-112200305283-7518348517",
+    "Cookie": "qgqp_b_id=5b9d8b5e63f579f82a73d131754bcbbb; AUTH_FUND.EASTMONEY.COM_GSJZ=AUTH*TTJJ*TOKEN; EMFUND1=null; EMFUND2=null; EMFUND3=null; EMFUND4=null; EMFUND5=null; EMFUND6=null; EMFUND7=null; EMFUND8=null; EMFUND0=null; st_si=38614788525230; st_asi=delete; ap_1_68c1f65b=1; EMFUND9=04-21 22:23:54@#$%u5929%u5F18%u4E2D%u8BC11000%u6307%u6570%u589E%u5F3AA@%23%24014201; st_pvi=34726450000364; st_sp=2023-04-16%2020%3A01%3A45; st_inirUrl=http%3A%2F%2Ffund.eastmoney.com%2Fdata%2Ffundranking.html; st_sn=3; st_psi=20250421222433451-112200305283-5103318229",
     "DNT": "1",
     "Host": "api.fund.eastmoney.com",
     "If-None-Match": "b56a685e-f638-4ccc-820d-c0495e577277",
     "Referer": "https://fundf10.eastmoney.com/",
     "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
 }
 
 
@@ -46,21 +46,51 @@ class FundDailyInfo:
 
 
 class FundDownloader:
-    url = "http://api.fund.eastmoney.com/f10/lsjz?callback=jQuery18304544093691286102_1567163891868&fundCode=%s&pageIndex=1&pageSize=%d&startDate=&endDate=&_=1567164070044"
+    base_url = "https://api.fund.eastmoney.com/f10/lsjz?callback=jQuery18306032015181749648_1745245483946&fundCode=%s&pageIndex=%d&pageSize=20&startDate=&endDate=&_=1745245484077"
     total_count = 0
     data = []  # sort by time inc
 
-    def __init__(self, fund_code: str, page_size=10000):
-        self.url = self.url % (fund_code, page_size)
+    def __init__(self, fund_code: str):
+        self.func_code = fund_code
+
         self._download_data()
 
     def _download_data(self):
+        total_count, page_size, first_page_data_list = self._download_page_data(1)
+
+        self.total_count = total_count
+        max_index = total_count
+        self.data = [FundDailyInfo("", 0, 0.0) for x in range(max_index)]
+
+        max_page = (total_count + page_size - 1) // page_size
+
+        data_list = first_page_data_list
+        for page in range(2, max_page + 1):
+            _, _, page_data_list = self._download_page_data(page, max_page)
+
+            data_list.extend(page_data_list)
+
+        for idx, item in enumerate(data_list):
+            index = max_index - idx - 1
+            if item["JZZZL"] == "":
+                item["JZZZL"] = "0.0"
+            if item["DWJZ"] == "":
+                item["DWJZ"] = "1.0"
+            self.data[index] = FundDailyInfo(item["FSRQ"], float(item["DWJZ"]), float(item["JZZZL"]))
+
+
+        print("\r", end="")
+
+    def _download_page_data(self, page: int, max_page=-1):
+        print(f"\rdownloading page {page}/{max_page}", end="")
+
         res = None
         for attempt in range(20):
+            page_url = self.base_url % (self.func_code, page)
             try:
-                res = requests.get(self.url, headers=headers)
+                res = requests.get(page_url, headers=headers)
             except Exception as e:
-                print("error during requests.get({}), attempt={}, e={}".format(self.url, attempt, e))
+                print("error during requests.get({}), attempt={}, e={}".format(page_url, attempt, e))
             else:
                 break
 
@@ -68,16 +98,11 @@ class FundDownloader:
         right_index = res.text.rfind(")")
         data = json.loads(res.text[left_index + 1:right_index])
 
-        self.total_count = data["TotalCount"]
-        max_index = min(data["TotalCount"], data["PageSize"])
-        self.data = [FundDailyInfo("", 0, 0.0) for x in range(max_index)]
-        for idx, item in enumerate(data["Data"]["LSJZList"]):
-            index = max_index - idx - 1
-            if item["JZZZL"] == "":
-                item["JZZZL"] = "0.0"
-            if item["DWJZ"] == "":
-                item["DWJZ"] = "1.0"
-            self.data[index] = FundDailyInfo(item["FSRQ"], float(item["DWJZ"]), float(item["JZZZL"]))
+        total_count = data["TotalCount"]
+        page_size = data["PageSize"]
+        data_list = data["Data"]["LSJZList"]
+
+        return total_count, page_size, data_list
 
     def print(self):
         for info in self.data:
